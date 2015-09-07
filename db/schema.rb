2bbc7d47e4abc42c6f8fe54a3b770406db33b725
Ellie Bartling <ellenbartling@gmail.com>
2015-09-07 08:56:16 -0500

    Got HTML back to working state by removing embedded ruby in todo lists view

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150906000807) do
+ActiveRecord::Schema.define(version: 20150907030521) do
 
-  create_table "todo_lists", force: :cascade do |t|
+  create_table "media_hits", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
